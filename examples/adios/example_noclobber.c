b2d06948b6485a8bc953148c3cbf1a59bba9c324
Tahsin Kurc <tmkurc@gmail.com>
2018-07-25 16:03:16 -0400

    Added code to handle PIO_NOCLOBBER. Code cleanup.

--- examples/adios/example_noclobber.c
+++ examples/adios/example_noclobber.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <mpi.h>
 #include <pio.h>
+#include <assert.h>
 #ifdef TIMING
 #include <gptl.h>
 #endif
@@ -9,6 +10,7 @@
 
 int main(int argc, char* argv[])
 {
+  int my_rank;
   int ntasks;
   int format[NUM_NETCDF_FLAVORS];
   int niotasks;
@@ -16,16 +18,16 @@ int main(int argc, char* argv[])
   int ioproc_start = 0;
   int iosysid;
   int ncid;
-  int dimid;
-  int varid;
   char filename[NC_MAX_NAME + 1];
   int num_flavors = 0;
+  int ret;
 
 #ifdef TIMING    
   GPTLinitialize();
 #endif   
 
   MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
   MPI_Comm_size(MPI_COMM_WORLD, &ntasks);
 
   niotasks = ntasks; 
@@ -46,32 +48,25 @@ int main(int argc, char* argv[])
   PIOc_set_iosystem_error_handling(iosysid, PIO_RETURN_ERROR, NULL);
 
   for (int fmt = 0; fmt < num_flavors; fmt++) {
-    sprintf(filename, "output_filename_%d.nc", fmt);
+    sprintf(filename, "output_noclobber_%d.nc", fmt);
 
-	printf("Opening file: %s by format: %d (%d) %d\n",filename,fmt,num_flavors,format[fmt]); fflush(stdout); 
-    int ierr = PIOc_createfile(iosysid, &ncid, &(format[fmt]), filename, PIO_CLOBBER);
-	printf("ERROR VALUE: %d\n",ierr);
-	if (ierr!=PIO_NOERR) {
-		printf("ERROR: File exists..\n"); fflush(stdout);
-		MPI_Barrier(MPI_COMM_WORLD);
-	} else {
-    	PIOc_def_dim(ncid, "filename_dim", 100, &dimid);
-    	PIOc_def_var(ncid, "filename", PIO_CHAR, 1, &dimid, &varid);
+    ret = PIOc_createfile(iosysid, &ncid, &(format[fmt]), filename, PIO_CLOBBER);
+    assert(ret == PIO_NOERR);
 
-    	PIOc_put_att(ncid, varid, "filename_att", PIO_CHAR, strlen(filename), filename);
-		char filname[256];
-		sprintf(filname,"testing_attributes");
-    	PIOc_put_att(ncid, varid, "filename_test", PIO_CHAR, strlen(filname), filname);
-		int test_val = 3;
-		PIOc_put_att(ncid, varid, "test_val", PIO_INT, 1, &test_val);
-    	PIOc_enddef(ncid);
+    ret = PIOc_closefile(ncid);
+    assert(ret == PIO_NOERR);
 
-		// PIOc_put_var_text(ncid,varid,"TESTING");
+    /* Since the file already exists (created above) and PIO_NOCLOBBER is used, an error code should be returned */
+    /* For ADIOS type, no error code is returned so far */
+    ret = PIOc_createfile(iosysid, &ncid, &(format[fmt]), filename, PIO_NOCLOBBER);
+    if (my_rank == 0) {
+      if (ret != PIO_NOERR)
+        printf("\nCreate file with clobber then no clobber failed as expected, returned code is %d, format is %d\n", ret, fmt);
+      else
+        printf("\nCreate file with clobber then no clobber did NOT failed as expected, returned code is %d, format is %d\n", ret, fmt);
+    }
 
-    	PIOc_closefile(ncid);
-	}
-	fflush(stdout);
-	MPI_Barrier(MPI_COMM_WORLD);
+    /* No close since createfile should fail */
   }
 
   PIOc_finalize(iosysid);
